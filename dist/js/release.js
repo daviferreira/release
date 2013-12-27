6f7647754e30ad4cb6bfa35201c44e9e54e7a22f
Davi Ferreira <contato@daviferreira.com>
2013-12-27 11:05:29 -0200

    Initial source file with grunt setup

---
+++ dist/js/release.js
@@ -0,0 +1,21 @@
+function MediumEditor(elements, options) {
+    'use strict';
+    return this.init(elements, options);
+}
+
+if (typeof module === 'object') {
+    module.exports = MediumEditor;
+}
+
+(function (window, document) {
+    'use strict';
+
+
+    MediumEditor.prototype = {
+        init: function init() {
+            return;
+        }
+    };
+
+
+}(window, document));
---
+++ src/js/release.js
@@ -0,0 +1,23 @@
+/*global console, module*/
+
+function MediumEditor(elements, options) {
+    'use strict';
+    return this.init(elements, options);
+}
+
+if (typeof module === 'object') {
+    module.exports = MediumEditor;
+}
+
+(function (window, document) {
+    'use strict';
+
+
+    MediumEditor.prototype = {
+        init: function init() {
+            return;
+        }
+    };
+
+
+}(window, document));
