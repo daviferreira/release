15cefec9cc12722eb0e588da1e221daeda4c3400
Davi Ferreira <davi.ferreira@corp.globo.com>
2014-01-10 13:32:02 -0200

    Implements very basic release functionality as a proof of concept

--- dist/js/release.js
+++ dist/js/release.js
@@ -10,12 +10,48 @@ if (typeof module === 'object') {
 (function (window, document) {
     'use strict';
 
-
     Release.prototype = {
-        init: function init() {
-            return;
-        }
-    };
 
+        init: function init(elements, options) {
+            this.options = options;
+            if (typeof elements === 'string') {
+                this.elements = document.querySelectorAll(elements);
+            } else {
+                this.elements = elements;
+            }
+            this.initElements();
+        },
+
+        initElements: function initElements() {
+            var i;
+            for (i = 0; i < this.elements.length; i += 1) {
+                this.bindRelease(i);
+            }
+        },
+
+        bindRelease: function bindRelease(index) {
+            var timer,
+                seconds = 0,
+                self = this;
+
+            this.elements[index].addEventListener('mousedown', function() {
+                timer = setInterval(function () {
+                    seconds += self.options.animationTime;
+                    console.log(seconds);
+                    if (seconds >= self.options.releaseTime) {
+                        clearInterval(timer);
+                        seconds = 0;
+                        console.log('pop');
+                    }
+                }, self.options.animationTime);
+            });
+
+            this.elements[index].addEventListener('mouseup', function() {
+                clearInterval(timer);
+                seconds = 0;
+            });
+        }
+
+    };
 
 }(window, document));
